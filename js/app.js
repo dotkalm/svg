a46d59b2777bf7fa6488e80be39e5d23bd806496
Joel <joelmholmberg@gmail.com>
2019-07-04 14:38:18 -0700

    speed change

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ class Line {
         const peakOne = setInterval(() => {
             this.peak1 += .013;
             // if(this.peak1 === 10000){clearInterval(peakOne)}
-            mainSvg.innerHTML=`<polyline  stroke="#808ff0" stroke-width="120" stroke-miterlimit="10" points="${this.peak1},${this.peak2} ${this.peak4},${this.peak1} ${this.peak5},${this.peak4} ${this.peak2},${this.peak4} ${this.peak4},${this.peak3}"/>`;
+            mainSvg.innerHTML=`<polyline  stroke="#808ff0" stroke-width="30" stroke-miterlimit="10" points="${this.peak1},${this.peak2} ${this.peak4},${this.peak1} ${this.peak5},${this.peak4} ${this.peak2},${this.peak4} ${this.peak4},${this.peak3}"/>`;
             
         }, 1);
 
@@ -23,10 +23,10 @@ class Line {
         // }
         const peakTwo = setInterval(() => {
             this.peak2-= .4
-            this.peak3+= .05;
+            this.peak3+= .15;
             this.peak4-= .024;
             this.peak5+= .2
-            }, 1);
+            }, 10);
 
         this.movePeak1();
         
