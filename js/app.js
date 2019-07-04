945c882e8fc9835233b1c8fe7991024ce1e5e17d
Joel <joelmholmberg@gmail.com>
2019-07-04 14:21:08 -0700

    speed change

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const mainDiv = document.querySelector('div');
 const mainSvg = document.querySelector('svg');
 
 class Line {
-    constructor(peak1=20, peak2=1000, peak3=0, peak4=1000, peak5=0){
+    constructor(peak1=20, peak2=-300, peak3=0, peak4=1000, peak5=0){
         this.peak1 = peak1;
         this.peak2 = peak2;
         this.peak3 = peak3;
@@ -12,20 +12,21 @@ class Line {
     }
     initLine(){
         const peakOne = setInterval(() => {
-            this.peak1 ++;
-            if(this.peak1 === 1000){clearInterval(peakOne)}
-            mainSvg.innerHTML=`<polyline  stroke="#808ff0" stroke-width="12" stroke-miterlimit="10" points="${this.peak1},${this.peak2} 34.25,${this.peak1} ${this.peak2},${this.peak4} ${this.peak2},${this.peak4} ${this.peak4},${this.peak3}"/>`;
+            this.peak1 += .013;
+            // if(this.peak1 === 10000){clearInterval(peakOne)}
+            mainSvg.innerHTML=`<polyline  stroke="#808ff0" stroke-width="120" stroke-miterlimit="10" points="${this.peak1},${this.peak2} ${this.peak4},${this.peak1} ${this.peak5},${this.peak4} ${this.peak2},${this.peak4} ${this.peak4},${this.peak3}"/>`;
             
-        }, 100);
+        }, 1);
 
         // for (let i=this.peak1; i<200; i++){
             
         // }
         const peakTwo = setInterval(() => {
-            this.peak2--
-            this.peak3+= 5;
-            this.peak4-= 4;
-            }, 100);
+            this.peak2-= .4
+            this.peak3+= .05;
+            this.peak4-= .024;
+            this.peak5+= .2
+            }, 1);
 
         this.movePeak1();
         
