c9572dcdeebd785fdc9614af3b440959073907d2
Joel <joelmholmberg@gmail.com>
2019-07-04 14:59:47 -0700

    random numbers

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const mainDiv = document.querySelector('div');
 const mainSvg = document.querySelector('svg');
 
 class Line {
-    constructor(peak1=20, peak2=-300, peak3=0, peak4=1000, peak5=0){
+    constructor(peak1=Math.random()*150, peak2=-Math.floor(Math.random()*300), peak3=0, peak4=800, peak5=Math.floor(Math.random()*1000)){
         this.peak1 = peak1;
         this.peak2 = peak2;
         this.peak3 = peak3;
@@ -14,7 +14,7 @@ class Line {
         const peakOne = setInterval(() => {
             this.peak1 += .013;
             // if(this.peak1 === 10000){clearInterval(peakOne)}
-            mainSvg.innerHTML=`<polyline  stroke="#808ff0" stroke-width="30" stroke-miterlimit="10" points="${this.peak1},${this.peak2} ${this.peak4},${this.peak1} ${this.peak5},${this.peak4} ${this.peak2},${this.peak4} ${this.peak4},${this.peak3}"/>`;
+            mainSvg.innerHTML=`<polyline  stroke="#808ff0" stroke-width="20" stroke-miterlimit="10" points="${this.peak1},${this.peak2} ${this.peak4},${this.peak1} ${this.peak5},${this.peak4} ${this.peak2},${this.peak4} ${this.peak4},${this.peak3}"/>`;
             
         }, 1);
 
@@ -23,10 +23,10 @@ class Line {
         // }
         const peakTwo = setInterval(() => {
             this.peak2-= .4
-            this.peak3+= .15;
+            this.peak3+= .05;
             this.peak4-= .024;
             this.peak5+= .2
-            }, 10);
+            }, 1);
 
         this.movePeak1();
         
